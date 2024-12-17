b90851eac3e0f314a606da7ef11c5894c089c2a9
Jonathan AP <109696746+Jonathan2390@users.noreply.github.com>
2024-12-16 22:13:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ if hist_button_a: # al hacer clic en el botón
 # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')   
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig_a = px.histogram(car_data, x="odometer")
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_a, use_container_width=True)
